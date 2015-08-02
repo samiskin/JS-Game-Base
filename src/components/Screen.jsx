4b37f9a6bcc22ed2a6e97158b7631122dd03041c
Shiranka Miskin <shiranka.miskin@gmail.com>
2015-08-02 00:59:34 -0400

    Completed CCD

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -47,15 +47,18 @@ export default class Screen extends Component {
     var target = new Vec2(400, 200);
     this.mouse = target;
 
-    this.tick();
-    this.tick();
-    this.tick();
     setTimeout(this.tick.bind(this), 1000/framerate);
   }
 
   tick() {
     this.ctx.clearRect(0, 0, this.props.width, this.props.height);
     this.arm.target = this.mouse;
+
+    this.ctx.beginPath();
+    this.ctx.arc(this.arm.target.x, this.arm.target.y, 2, 0, 2*Math.PI);
+    this.ctx.stroke();
+
+
     this.arm.tick();
     this.arm.draw(this.ctx);
     setTimeout(this.tick.bind(this), 1000/framerate);
